59e6e5ad8123d35cb9dff20a2b5bb8a63eb67e50
jnsagai <jnsagai@gmail.com>
2020-09-27 16:43:57 +0100

    - Enhance clone.py documentation

--- clone.py
+++ clone.py
@@ -17,26 +17,30 @@ import math
 import random
 import matplotlib.pyplot as plt
 
+# Read all lines from the driving csv file
 samples  = []
 with open('training_data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
 
-
+# Slip the train and validation samples using the rate of 80% / 20%
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+# Since the whole samples images size are pretty huge ( more than 10GB!!)
+# lets use a generator with batches to progressively load data to the training
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         random.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
             images = []
             measurements = []
             for batch_sample in batch_samples:
                 steering_center = float(batch_sample[3])
+                # Lets use both the right and left images for training the model
+                # and consider a correction factor for them
                 correction = 0.2
                 steering_left = steering_center + correction
                 steering_right = steering_center - correction
@@ -51,6 +55,8 @@ def generator(samples, batch_size=32):
                 measurements.append(steering_left)
                 measurements.append(steering_right)
             
+            # In order to augment the input samples, we are going to duplicate
+            # all the input images and flip them
             augmented_images, augmented_measurements = [], []
             for image, measurements in zip(images, measurements):
                 augmented_images.append(image)
@@ -58,7 +64,6 @@ def generator(samples, batch_size=32):
                 augmented_images.append(cv2.flip(image, 1))
                 augmented_measurements.append(measurements*-1.0)
                 
-            # trim image to only see section with road
             X_train = np.array(augmented_images)
             y_train = np.array(augmented_measurements)
             
@@ -71,7 +76,7 @@ batch_size=64
 train_generator = generator(train_samples, batch_size=batch_size)
 validation_generator = generator(validation_samples, batch_size=batch_size)
 
-
+# Model architecture
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
@@ -109,4 +114,3 @@ plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.show()
-
